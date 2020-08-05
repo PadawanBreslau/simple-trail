0c81b4ec84b015132019ecb6bb600e12eb6053a0
Staszek <s.zawadzki@visuality.pl>
2020-08-05 12:11:29 +0200

    Straightening the trail

---
+++ lib/simple_trail/manipulation/straightener.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module Manipulation
+  class Straightener
+    def initialize(points)
+      @points = points
+      @latlng_points = @points.map{|p| Geokit::LatLng.new(p[:lat], p[:lon])}
+    end
+
+    def points_without_clusters
+      @average_distance_between ||= average_distance_between
+      @possible_clusters = detect_clusters
+      smoothe_points!
+    end
+
+    def smoothe_points!
+      group_clusters
+      straighten_points!
+    end
+
+    private
+
+    BASIC_SAMPLE_SIZE = 100
+    BASIC_CLUSTER_CHECK_SIZE = 10
+    BASIC_CLUSTER_CHECK_RATIO = 0.3
+
+    def group_clusters
+      @cluster_groups = []
+      @possible_clusters.each do |index|
+        @group ||= []
+        if index.nil?
+          unless @group.empty?
+            @cluster_groups << @group
+            @group = []
+          end
+          next
+        else
+          @group << index
+        end
+      end
+      @cluster_groups << @group unless @group.empty?
+    end
+
+    def straighten_points!
+      @cluster_groups.each do |cluster_group|
+        cluster_coords = @points.values_at(*cluster_group)
+        avg_lat = cluster_coords.map{|cc| cc[:lat].to_f}.inject(:+) / cluster_coords.size
+        avg_lon = cluster_coords.map{|cc| cc[:lon].to_f}.inject(:+) / cluster_coords.size
+        @points[cluster_group.first][:lat] = avg_lat.to_s
+        @points[cluster_group.first][:lon] = avg_lon.to_s
+        cluster_group.drop(1).each{|index| @points.delete_at(index)}
+      end
+    end
+
+    def average_distance_between
+      sample_points = (0..@points.size - 2).to_a.sample(BASIC_SAMPLE_SIZE)
+      sample_points.map do |i|
+        start = @latlng_points[i]
+        finish = @latlng_points[i+1]
+        start.distance_to(finish)
+      end.inject(:+) / sample_points.size
+    end
+
+    def detect_clusters
+      (0..@points.size - BASIC_CLUSTER_CHECK_SIZE - 1).to_a.map do |i|
+        start = @latlng_points[i]
+        finish = @latlng_points[i + BASIC_CLUSTER_CHECK_SIZE]
+        distance = start.distance_to(finish)
+        i if distance < @average_distance_between * BASIC_CLUSTER_CHECK_SIZE * BASIC_CLUSTER_CHECK_RATIO
+      end
+    end
+  end
+end
+
---
+++ spec/manipulation/straightener_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Manipulation::Straightener do
+  it 'should calculate basic file info' do
+    parser = Parser::Gpx.new('./spec/examples/test_1.gpx')
+    parser.read
+
+    straigtener_logic = described_class.new(parser.points)
+    straigtener_logic.points_without_clusters
+  end
+end
